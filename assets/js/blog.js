f034a9c3c48c3d7d0ef11f322f6c56b40f645910
Ernesto Gonzalez <ernestoale37@gmail.com>
2024-05-25 16:47:28 -0400

    Basic structure and functionality added, dark/light mode operational

---
+++ assets/js/blog.js
@@ -0,0 +1,8 @@
+var Switch = document.getElementById("switch");
+
+function switchMode() {
+    var mode = document.body;
+    mode.classList.toggle("dark");
+}
+
+Switch.addEventListener('click', switchMode);
