3d9cac6ea84237ef6086cc3e07e70fcbc6dafaf3
Ernesto Gonzalez <ernestoale37@gmail.com>
2024-06-07 00:28:15 -0400

    Improved local storage logic that iterates through array and saves blogs for later

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,6 +1,7 @@
 const Switch = document.getElementById("switch");
 const Submit = document.querySelector("form");
 var log = JSON.parse(localStorage.getItem('blog#'));
+var BlogC = JSON.parse(localStorage.getItem('blog'));
 
 function switchMode() {
     var mode = document.body;
@@ -10,25 +11,50 @@ function switchMode() {
 Switch.addEventListener('click', switchMode);
 Submit.addEventListener('submit', function(event) {
     event.preventDefault();
+
     var Name = document.getElementById("name");
     var title = document.getElementById("title");
     var content = document.getElementById("content");
+
+    if (log != null){
+        log += 1;
+        localStorage.setItem('blog#', JSON.stringify(log));
+        if (log > 5) {
+            log = 1;
+            localStorage.setItem('blog#', JSON.stringify(log));
+        }
+    } else {
+        log = 1;
+        localStorage.setItem('blog#', JSON.stringify(log));
+    }
+    if (Name.value == "") {
+        window.alert("Please, add missing information before submitting blog.");
+        return;
+    } else if (title.value == "") {
+        window.alert("Please, add missing information before submitting blog.");
+        return;
+    } else if (content.value == "") {
+        window.alert("Please, add missing information before submitting blog.");
+        return;
+    }
+
     var blog = {
         "Name": Name.value,
         "Title": title.value,
         "Content": content.value,
     }
-    localStorage.setItem('blog', JSON.stringify(blog));
-    if (log != null){
-        log += 1;
-        localStorage.setItem('blog#', JSON.stringify(log));
-    } else if (log == 5){
-        log = 1;
-        localStorage.setItem('blog#', JSON.stringify(log));
+    if (BlogC != null) {
+        var Blog = [];
+        for (let i = 0; i < log - 1; i++) {
+            Blog.push(BlogC[i]);
+        }
+        Blog.push(blog);
+        localStorage.setItem('blog', JSON.stringify(Blog));
+    } else {
+        var Blog = [];
+        Blog.push(blog);
+        localStorage.setItem('blog', JSON.stringify(Blog));
     }
-    else {
-        log = 1;
-        localStorage.setItem('blog#', JSON.stringify(log));
-    }
+    
     window.open('blog.html', '_self');
 });
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,9 +1,11 @@
 var blog = JSON.parse(localStorage.getItem('blog'));
 var log = JSON.parse(localStorage.getItem('blog#'));
 
-if (log != 0) {
-    var post = document.getElementById(JSON.stringify(log));
-    post.getElementsByClassName('name')[0].textContent = blog.Name;
-    post.getElementsByClassName('title')[0].textContent = blog.Title;
-    post.querySelector('p').textContent = blog.Content;
+if (log != null) {
+    for (let i = 0; i < log; i++) {
+        var post = document.getElementById(JSON.stringify(i + 1));
+        post.getElementsByClassName('name')[0].textContent = blog[i].Name;
+        post.getElementsByClassName('title')[0].textContent = blog[i].Title;
+        post.querySelector('p').textContent = blog[i].Content;
+    }
 }
